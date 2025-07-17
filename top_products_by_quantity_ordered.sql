5f03573c36d321cf92131659ca0db192d977f634
nyerogude <ogudeoren@gmail.com>
2025-07-17 12:02:48 +0100

    Refacto: update code with product name

--- top_products_by_quantity_ordered.sql
+++ top_products_by_quantity_ordered.sql
@@ -1,7 +1,9 @@
 SELECT 
-	product_id,
-	SUM(quantity_ordered) total_units_ordered
-FROM procurement_orders
+	po.product_id product_id,
+     p.product_name product_name,
+	SUM(po.quantity_ordered) total_units_ordered
+FROM procurement_orders po
+JOIN products p ON po.product_id=p.product_id
 GROUP BY product_id
 ORDER BY SUM(quantity_ordered) DESC;
 
