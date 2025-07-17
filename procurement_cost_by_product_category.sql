9711f0bf031cfc5b473cf7d5d4f0a8ebd74bde6f
nyerogude <ogudeoren@gmail.com>
2025-07-17 15:01:51 +0100

    Refactor:update code with percent of total spend per category

--- procurement_cost_by_product_category.sql
+++ procurement_cost_by_product_category.sql
@@ -1,10 +1,16 @@
+WITH procurement_cost_by_category AS (
+    SELECT 
+        p.category,
+        SUM(po.quantity_ordered * po.unit_cost) AS procurement_cost
+    FROM products p 
+    JOIN procurement_orders po 
+        ON p.product_id = po.product_id
+    GROUP BY p.category
+)
+
 SELECT 
-	p.category category,
-    SUM(po.quantity_ordered*po.unit_cost) procurement_cost
-FROM products p 
-JOIN procurement_orders po ON p.product_id=po.product_id
-GROUP BY p.category
+    category,
+    procurement_cost,
+    ROUND((procurement_cost / SUM(procurement_cost) OVER()) * 100, 2) AS pct_of_total_spend
+FROM procurement_cost_by_category
 ORDER BY procurement_cost DESC;
-
-
-
